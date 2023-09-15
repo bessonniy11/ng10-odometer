def47f0db13cec86df375428ad4d1b3641ada6a9
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2023-09-15 14:34:08 +0300

    Ng10OdometerModule to any

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
 export declare class Ng10OdometerModule {
-    static forRoot(): ModuleWithProviders<Ng10OdometerModule>;
+    static forRoot(): ModuleWithProviders<any>;
 }
